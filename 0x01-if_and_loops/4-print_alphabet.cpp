be1555a81981a6f76c22649fc397420e23f3c73b
Victor Zuluaga <zrvictor@outlook.com>
2020-07-10 12:21:34 -0500

    print alphabet in C++ with putchar

---
+++ 0x01-if_and_loops/4-print_alphabet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/**
+ * main - Enter point
+ * 
+ * Return: Always 0
+ */
+
+int main()
+{
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+putchar('\n');
+return (0);
+}

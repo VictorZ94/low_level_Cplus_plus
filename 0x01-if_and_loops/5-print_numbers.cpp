77ee45fd9d849a404a66ca798294f3926b64521f
Victor Zuluaga <zrvictor@outlook.com>
2020-07-27 20:36:37 -0500

    print natural numbers 0 ... 9

---
+++ 0x01-if_and_loops/5-print_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "prototype.h"
+
+/**
+ * main - Enter pointer function
+ * printf the numbers natural 
+ * 
+ * Return: Always 0 
+ */
+
+using namespace std;
+int main()
+{
+      int i = 0;
+      while (i < 10)
+      {
+            _putchar(i + '0');
+            i++;
+      }
+_putchar('\n');
+return (0);
+}
